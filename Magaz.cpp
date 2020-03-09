472f8b2d0035732bb64aea3ca5ee136a5453328e
Png Wulf <deucelives2000@gmail.com>
2020-03-09 20:36:29 +0300

    Конец

--- Magaz.cpp
+++ Magaz.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <cstdlib>
 #include <conio.h>
 #include <fstream>
@@ -8,16 +7,15 @@
 #include <iomanip>
 #include <iostream>
 
-
 using namespace std;
 
 class Product
 {
-public:
+private:
     double number;
     double weight;
     double price;
-
+public:
     void print()
     {
         cout << number << " - ID товара" << endl << weight << " - Вес товара" << endl << price << "р.- Цена\n" << endl;
@@ -36,21 +34,18 @@ public:
     }
     void set_number(double number)
     {
-        this->number;
-        if (number > 0)
-            return;
+        if ( number >= 0)
+            this->number = number;
     }
     void set_weight(double weight)
     {
-        this->weight;
-        if (weight > 0)
-            return;
+        if (weight >= 0)
+            this->weight = weight;
     }
     void set_price(double price)
     {
-        this->price;
-        if (price > 0)
-            return;
+        if (price >= 0)
+            this->price = price;
     }
     Product()
     {
@@ -75,21 +70,26 @@ public:
     {
         count = 0;
     }
-    bool Add(int number, double weight, double price)
+    bool Add(double number, double weight, double price)
     {
-        if (count >= 1000)
-            return false;
-        P[count].number = number;
-        P[count].weight = weight;
-        P[count].price = price;
-        count++;
+        if (count <= 1000)
+        {
+            P[count].set_number(number);
+            P[count].set_weight(weight);
+            P[count].set_price(price);
+            count++;
+            return true;
+        }
+        return false;
     }
     bool Del()
     {
-        if (count < 0)
-            return false;
-        count--;
-        return true;
+        if (count > 0)
+        {
+            count--;
+            return true;
+        }
+        return false;
     }
     void print()
     {
@@ -143,8 +143,14 @@ int main(int argc, char* argv[])
             break;
         case '2':
             system("cls");
-            market->Del();
-            cout << "Товар удалён" << endl;
+            if (market->Del() == true)
+            {
+                cout << "Товар удалён" << endl;
+            }
+            else
+            {
+                cout << "Ошибка, нечего удалять" << endl;
+            }
             break;
         case '3':
             system("cls");
@@ -179,127 +185,3 @@ int main(int argc, char* argv[])
     system("pause");
     return 0;
 }
-
-/* int A, A1;                       //Меню с помощью if
- int number;
- double weight;
- double price;
- cout << "Инструкция" << endl;
- cout << "1 - Добавить товар" << endl;
- cout << "2 - Удалить товар" << endl;
- cout << "3 - Просмотр товара" << endl;
- cout << "4 - Выход из приложения" << endl;
- cout << "" << endl;
- cout << "" << endl;
- cout << "" << endl;
- cout << "Меню" << endl;
- cout << "1. Добавить товар" << endl;
- cout << "2. Удалить товар" << endl;
- cout << "3. Просмотр товаров" << endl;
- cout << "4. Выход" << endl;
- cout << "" << endl;
- cout << "" << endl;
- cout << "" << endl;
- cin >> A;
- if (A == 1)
- {
-     system("cls");
-     cout << "" << endl;
-     cout << "" << endl;
-     cout << "Добавить товар:" << endl;
-     cout << "ID товара" << endl;
-     cin >> number;
-     cout << "Вес товара" << endl;
-     cin >> weight;
-     cout << "Цена товара" << endl;
-     cin >> price;
-     cout << "Товар добавлен, нажмите 5" << endl;
-     cin >> A1;
-     if (A1 == 5)
-     {
-         system("cls");
-         cout << "" << endl;
-         cout << "" << endl;
-         cout << "Меню" << endl;
-         cout << "1. Добавить товар" << endl;
-         cout << "2. Удалить товар" << endl;
-         cout << "3. Просмотр товаров" << endl;
-         cout << "4. Выход" << endl;
-         cout << "" << endl;
-         cout << "" << endl;
-         cin >> A;
-     }
- }
- if (A == 2)
- {
-     system("cls");
-     cout << "" << endl;
-     cout << "" << endl;
-     market.Del();
-     cout << "Товар удалён" << endl;
-     cout << "Нажмите 6" << endl;
-     cin >> A1;
-     if (A1 == 6)
-     {
-         system("cls");
-         cout << "Меню" << endl;
-         cout << "1. Добавить товар" << endl;
-         cout << "2. Удалить товар" << endl;
-         cout << "3. Просмотр товаров" << endl;
-         cout << "4. Выход" << endl;
-         cout << "" << endl;
-         cout << "" << endl;
-         cin >> A;
-
-     }
- }
- if (A == 3)
- {
-     system("cls");
-     cout << "" << endl;
-     cout << "" << endl;
-     cout << "товары:" << endl;
-     market.Add(number, weight, price);
-     market.print();
-     cout << "Нажмите 7" << endl;
-     cin >> A1;
-     if (A1 == 7)
-     {
-         system("cls");
-         cout << "Меню" << endl;
-         cout << "1. Добавить товар" << endl;
-         cout << "2. Удалить товар" << endl;
-         cout << "3. Просмотр товаров" << endl;
-         cout << "4. Выход" << endl;
-         cout << "" << endl;
-         cout << "" << endl;
-         cin >> A;
-
-     }
- }
- if (A == 4)
- {
-     exit(0);
- }*/
- /*cout << "Dobavit' na sklad" << endl;
- cout << "ID tovara" << endl;
- cin >> number;
- cout << "Ves tovara" << endl;
- cin >> weight;
- cout << "Cena tovara" << endl;
- cin >> price;
- cout << "Dobavilos' :" << endl;*/
- //cout << "Dobavilos' :" << endl << number << " - ID tovara" << endl << weight << " - Ves tovara" << endl << price << "r.- Cena" << endl;
- /*market.Add(number, weight, price);
- market.print();*/
- /*cout << "Konstruktor po defolty" << endl;
- Product product = Product();                                                // Конструкторы
- product.print();
- cout << "Konstruktor s parametrami" << endl;
- Product product1 = Product(567834, 350.35, 7000);
- product1.print();
- Market market;
- market.Add(564366, 340, 6800);
- market.Add(564367, 400, 8000);
- market.Add(564368, 300, 6500);
- market.print();*/
